4498286e32a1562cef396c78e4a439c2bf90e60c
Hawre <131952165+0xHawre@users.noreply.github.com>
2024-09-11 22:58:57 +0330

    Update CAT20.sh

--- CAT20.sh
+++ CAT20.sh
@@ -2,6 +2,8 @@
 
 sudo apt update && sudo apt install -y curl && curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
 
+dpkg -l | grep -q '^ii  curl ' || sudo apt-get install -y curl && dpkg -l | grep -q '^ii  jq ' || sudo apt-get install -y jq
+
 sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
