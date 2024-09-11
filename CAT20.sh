f07141a7c4d6c4fc466ea8fc96cd0bbd11d3ea2d
Hawre <131952165+0xHawre@users.noreply.github.com>
2024-09-11 22:55:54 +0330

    Update CAT20.sh

--- CAT20.sh
+++ CAT20.sh
@@ -1,82 +1,65 @@
 #!/bin/bash
 
+sudo apt update && sudo apt install -y curl && curl -fsSL https://get.docker.com -o get-docker.sh && sudo sh get-docker.sh
 
-sudo apt-get update
-
-check_and_install_docker() {
-    if ! command -v docker &> /dev/null; then
-        echo "Docker is not installed. Installing Docker..."
-        
-        sudo apt-get update
-        sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-        sudo apt-get update
-        sudo apt-get install -y docker-ce
-
-        echo "Docker has been installed."
-    else
-        echo "Docker is already installed."
-    fi
-}
-
-check_and_install_docker
-
-VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
-
-DESTINATION=/usr/local/bin/docker-compose
-sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
-sudo chmod 755 $DESTINATION
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep 'tag_name' | cut -d\" -f4)/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
 sudo apt-get install npm -y
 
-sudo npm install n -g
+sudo npm i n -g
+
 sudo n stable
+
 sudo npm i -g yarn
 
-git clone https://github.com/CATProtocol/cat-token-box
-cd cat-token-box
+git clone https://github.com/CATProtocol/cat-token-box && cd cat-token-box
+sudo chown -R $USER:$USER ~/cat-token-box
 
-sudo yarn install && sudo yarn build
+sudo yarn install && yarn build
 
-cd ./packages/tracker/
+cd packages/tracker
 
-sudo chmod 777 docker/data
-sudo chmod 777 docker/pgdata
+sudo chmod 777 docker/data && sudo chmod 777 docker/pgdata && sudo docker compose up -d
 
-sudo docker-compose up -d
+cd $HOME/cat-token-box && sudo docker build -t tracker:latest .
 
-cd ../../
-
-sudo docker build -t tracker:latest .
+read -p "node name : vname
 
 sudo docker run -d \
-    --name tracker \
+  --name $(vname) \
     --add-host="host.docker.internal:host-gateway" \
     -e DATABASE_HOST="host.docker.internal" \
     -e RPC_HOST="host.docker.internal" \
     -p 3000:3000 \
     tracker:latest
 
+cd $HOME/cat-token-box/packages/cli
+
+cat <<EOF > config.json
+{
+  "network": "fractal-mainnet",
+  "tracker": "http://127.0.0.1:3000",
+  "dataDir": ".",
+  "maxFeeRate": 1000,
+  "rpc": {
+      "url": "http://127.0.0.1:8332",
+      "username": "bitcoin",
+      "password": "opcatAwesome"
+  }
+}
+EOF
+
 sudo yarn cli wallet create
 
-sudo yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5
+sudo yarn cli wallet address
 
-command="sudo yarn cli mint -i 45ee725c2c5993b3e4d308842d87e973bf1951f5f7a804b21e4dd964ecd12d6b_0 5"
 
-while true; do
-    $command
+echo "Do you want to see your seed? (yes/no)"
+read answer
 
-    if [ $? -ne 0 ]; then
-        echo "Command execution failed, exiting loop"
-        exit 1
-    fi
-
-    sleep 1
-done
-
-chmod +x script.sh
-
+if [ "$answer" = "yes" ]; then
+    cat wallet.json
+else
+    echo "Continuing without showing the seed."
+fi
